e00850076eac351eead37122f45e293c979b9e55
GloomySunday <121055230@qq.com>
2017-04-28 16:42:28 +0800

    Initial Commit

---
+++ TMRouter/TMRouter.h
@@ -0,0 +1,19 @@
+//
+//  TMRouter.h
+//  TMRouter
+//
+//  Created by 孟钰丰 on 2017/4/28.
+//  Copyright © 2017年 GloomySunday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMRouter.
+FOUNDATION_EXPORT double TMRouterVersionNumber;
+
+//! Project version string for TMRouter.
+FOUNDATION_EXPORT const unsigned char TMRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMRouter/PublicHeader.h>
+
+
---
+++ TMRouterTests/TMRouterTests.swift
@@ -0,0 +1,36 @@
+//
+//  TMRouterTests.swift
+//  TMRouterTests
+//
+//  Created by 孟钰丰 on 2017/4/28.
+//  Copyright © 2017年 GloomySunday. All rights reserved.
+//
+
+import XCTest
+@testable import TMRouter
+
+class TMRouterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
